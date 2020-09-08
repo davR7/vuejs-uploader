614cdaa3b88b48bbe8e270ed5d4cf7f033056337
Davidson Ratis <davidsonmakarios@gmail.com>
2020-09-08 15:45:18 -0300

    first commit

---
+++ src/components/VueDropzoneX.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vue-dropzone-x">
+      <h2>VueDropzoneX</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'VueDropzoneX'
+}
+</script>
+
+<style>
+
+</style>
